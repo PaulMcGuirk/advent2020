ad9737386063751029d111c55c510c090d1be98f
Paul McGuirk <>
2020-12-02 05:31:41 -0600

    Add Day 2

---
+++ Advent/Advent.Text/Solvers/Day02Part01.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("2.1")]
+    public class Day02Part01 : Solver
+    {
+        private static Regex _passwordRx = new Regex(@"^(?<min>\d+)-(?<max>\d+) (?<reqCh>[a-z]): (?<password>[a-z]+)$");
+
+        public override object Solve()
+        {
+            var contents = ReadInputFile();
+            return contents.Split('\n')
+                .Where(IsValidPasswordDatabaseLine)
+                .Count();
+        }
+
+        private bool IsValidPasswordDatabaseLine(string line)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                return false;
+            }
+
+            var match = _passwordRx.Match(line);
+            var min = int.Parse(match.Groups["min"].Value);
+            var max = int.Parse(match.Groups["max"].Value);
+            var reqCh = match.Groups["reqCh"].Value[0];
+            var password = match.Groups["password"].Value;
+
+            var requiredCharCount = password.Where(ch => ch == reqCh).Count();
+            return (min <= requiredCharCount) && (requiredCharCount <= max);
+        }
+    }
+}
---
+++ Advent/Advent.Text/Solvers/Day02Part02.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("2.2")]
+    public class Day02Part02 : Solver
+    {
+        private static Regex _passwordRx = new Regex(@"^(?<posA>\d+)-(?<posB>\d+) (?<reqCh>[a-z]): (?<password>[a-z]+)$");
+
+        public override object Solve()
+        {
+            var contents = ReadInputFile();
+            return contents.Split('\n')
+                .Where(IsValidPasswordDatabaseLine)
+                .Count();
+        }
+
+        private bool IsValidPasswordDatabaseLine(string line)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                return false;
+            }
+
+            var match = _passwordRx.Match(line);
+            var posA = int.Parse(match.Groups["posA"].Value);
+            var posB = int.Parse(match.Groups["posB"].Value);
+            var reqCh = match.Groups["reqCh"].Value[0];
+            var password = match.Groups["password"].Value;
+
+            return (password[posA - 1] == reqCh) ^ (password[posB - 1] == reqCh);
+        }
+    }
+}
