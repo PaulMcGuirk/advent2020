5b046c3c7ec9e00baae07119e464d51a8d93fff6
Paul McGuirk <>
2020-12-06 05:32:12 -0600

    Put the right class in the right file

--- Advent/Advent.Text/Solvers/Day06Part01.cs
+++ Advent/Advent.Text/Solvers/Day06Part01.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Advent.Text.TravelDocuments;
 
 namespace Advent.Text.Solvers
 {
-    [Solver("6.2")]
-    public class Day06Part02 : Solver
+    [Solver("6.1")]
+    public class Day06Part01 : Solver
     {
         public override object Solve()
         {
@@ -12,33 +13,8 @@ namespace Advent.Text.Solvers
 
             var result = contents.Trim()
                 .Split("\n\n")
-                .Select(group => CollectionIntersection(group.Split('\n')).Count())
+                .Select(group => new HashSet<char>(group.Replace("\n", "")).Count())
                 .Sum();
-
-            return result;
-        }
-
-        /// <summary>
-        /// Find the intersection of a collection of collections
-        /// </summary>
-        /// <typeparam name="T">The type of element in each collection</typeparam>
-        /// <param name="collections">The collection of collections</param>
-        /// <returns>The intersection</returns>
-        private static IEnumerable<T> CollectionIntersection<T>(IEnumerable<IEnumerable<T>> collections)
-        {
-            if (!collections.Any())
-            {
-                return Enumerable.Empty<T>();
-            }
-
-            var enumerator = collections.GetEnumerator();
-            enumerator.MoveNext();
-            var result = enumerator.Current.ToHashSet();
-
-            while (enumerator.MoveNext())
-            {
-                result.IntersectWith(enumerator.Current);
-            }
 
             return result;
         }
