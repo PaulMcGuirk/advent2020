d46d104291edfff306ebb0d652038577ef6888a8
Paul McGuirk <>
2020-12-06 05:23:37 -0600

    Solutions to day 6

---
+++ Advent/Advent.Text/Solvers/Day06Part01.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("6.2")]
+    public class Day06Part02 : Solver
+    {
+        public override object Solve()
+        {
+            var contents = ReadInputFile();
+
+            var result = contents.Trim()
+                .Split("\n\n")
+                .Select(group => CollectionIntersection(group.Split('\n')).Count())
+                .Sum();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Find the intersection of a collection of collections
+        /// </summary>
+        /// <typeparam name="T">The type of element in each collection</typeparam>
+        /// <param name="collections">The collection of collections</param>
+        /// <returns>The intersection</returns>
+        private static IEnumerable<T> CollectionIntersection<T>(IEnumerable<IEnumerable<T>> collections)
+        {
+            if (!collections.Any())
+            {
+                return Enumerable.Empty<T>();
+            }
+
+            var enumerator = collections.GetEnumerator();
+            enumerator.MoveNext();
+            var result = enumerator.Current.ToHashSet();
+
+            while (enumerator.MoveNext())
+            {
+                var next = enumerator.Current;
+                result.IntersectWith(next);
+            }
+            return result;
+        }
+    }
+}
---
+++ Advent/Advent.Text/Solvers/Day06Part02.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advent.Text.TravelDocuments;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("6.1")]
+    public class Day06Part01 : Solver
+    {
+        public override object Solve()
+        {
+            var contents = ReadInputFile();
+
+            var result = contents.Trim()
+                .Split("\n\n")
+                .Select(group => new HashSet<char>(group.Replace("\n", "")).Count())
+                .Sum();
+
+            return result;
+        }
+    }
+}
