0d29cd714fe2a59a5ad5e3743f0a0788a69ddd23
Paul McGuirk <>
2020-12-10 05:20:38 -0600

    Add solutions for day 10

---
+++ Advent/Advent.Text/Solvers/Day10Part01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Advent.Text.Security;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("10.1")]
+    public class Day10Part01 : Solver
+    {
+        public override object Solve()
+        {
+            var contents = ReadInputFile();
+
+            var adapters = contents.Trim()
+                .Split('\n')
+                .Select(line => int.Parse(line))
+                .ToList();
+
+            adapters.Sort();
+
+            var differences = new List<int> { adapters[0] }; // the wall has effectively 0 jolts
+            differences.AddRange(Enumerable.Range(1, adapters.Count - 1)
+                .Select(i => adapters[i] - adapters[i - 1]));
+            differences.Add(3); // the built-in is always 3 jolts higher than the max
+
+            var counts = Enumerable.Range(0, 4)
+                .Select(joltDifference => differences.Count(x => x == joltDifference))
+                .ToList();
+
+            return counts[1] * counts[3];
+        }
+    }
+}
---
+++ Advent/Advent.Text/Solvers/Day10Part02.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("10.2")]
+    public class Day10Part02 : Solver
+    {
+        public override object Solve()
+        {
+            var contents = ReadInputFile();
+
+            var adapters = contents.Trim()
+                .Split('\n')
+                .Select(line => int.Parse(line))
+                .ToList();
+
+            adapters.Sort();
+            var builtIn = adapters.Last() + 3; // the built in adapeter is always 3 more jolts than the max in bag
+            adapters.Add(builtIn);
+
+            // counts will hold the number of ways to end with the
+            // adapter of the given joltage
+            // since there's only 1 wall, there's one way to end with the wall
+            var counts = new Dictionary<int, long> { [0] = 1 };
+
+            foreach (var adapter in adapters)
+            {
+                // We can put this adapeter at the end of any valid chain of
+                // adapters, so the number of ways to end with this adapter
+                // is jut the total number of chains that this adapter
+                // can be added to
+                counts[adapter] = Enumerable.Range(1, 3)
+                    .Select(diff => counts.GetValueOrDefault(adapter - diff))
+                    .Sum();
+            }
+
+            return counts[builtIn];
+        }
+    }
+}
