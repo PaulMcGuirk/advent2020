abe66236b4b1a44ce6ee3a2d5829e4ff1f0ac6b5
Paul McGuirk <>
2020-12-13 07:10:35 -0600

    Add solutions for day 13

---
+++ Advent/Advent.Text/Solvers/Day13Part01.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("13.1")]
+    public class Day13Part01 : Solver
+    {
+        public override object Solve()
+        {
+            var rawInput = ReadInputFile();
+
+            var lines = rawInput.Trim().Split('\n');
+            var earliestDepartTime = int.Parse(lines[0]);
+            var busIds = lines[1].Split(',')
+                .Where(piece => piece != "x")
+                .Select(int.Parse);
+
+            var earliest = busIds
+                .Select(busId => (busId, waitTime: WaitTime(earliestDepartTime, busId)))
+                .Aggregate((min, next) => next.waitTime < min.waitTime ? next : min);
+
+            return earliest.busId * earliest.waitTime;
+        }
+
+        /// <summary>
+        /// Get the amount of time to wait for a particular bus
+        /// </summary>
+        /// <param name="earliestDepartTime">The earliest you want to leave</param>
+        /// <param name="busId">The bus ID to wait for</param>
+        /// <returns>The amount of time you need to wait</returns>
+        private int WaitTime(int earliestDepartTime, int busId)
+        {
+            var remainder = earliestDepartTime % busId;
+
+            return remainder == 0 ? 0 : busId - remainder;
+        }
+    }
+}
---
+++ Advent/Advent.Text/Solvers/Day13Part02.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("13.2")]
+    public class Day13Part02 : Solver
+    {
+        public override object Solve()
+        {
+            var rawInput = ReadInputFile();
+
+            var lines = rawInput.Trim().Split('\n');
+            var pieces = lines[1].Split(',')
+                .Select((piece, pos) => (piece, pos))
+                .Where(pair => pair.piece != "x")
+                .Select(pair => (int.Parse(pair.piece), pair.pos))
+                .ToList();
+
+            var result = Contest(pieces);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Given a list of primes and offsets, finds the smallest number
+        /// that for each (prime, offset) pair, is smaller than a multiple
+        /// of that prime by that offset.
+        /// </summary>
+        /// <remarks>The offset can be larger than the prime itself. For example,
+        /// we might have (13, 25) meaning that we're looking for a number that's
+        /// 25 less than a multiple of 13. It's useful that that number will also
+        /// be 12 less than a multiple of 13.</remarks>
+        /// <example>[(7, 0), (13, 1) (59, 4)] returns 1715 since that is the
+        /// smallest number that is divisible by 7, is 1 less than a multiple
+        /// of 13, and is 4 less than a multiple of 59</example>
+        /// <param name="primesWithOffsets">The (prime, offset) pairs</param>
+        /// <returns>The first number that satisfies this</returns>
+        private long Contest(List<(int prime, int offset)> primesWithOffsets)
+        {
+            var result = 0L;
+            var product = 1L;
+
+            // let p1, ..., pk be the primes we're considering
+            // we'll write our number as
+            // n = a1 + a2 * p1 + a3 * (p2 * p1) + ... + ak * p(k-1) * p(k-2) * ... * p1
+            // so that every term is divisible by pi except the first i terms
+            // The first offset condition (i.e. involving p1) can then be satisfied by setting a1 alone.
+            // Once a1 is set, the second offset condition (i.e. involving p2) can be satisfied
+            // by finding a value of a2. In this way, we iteratively get the
+            // numbers ak and so get the answer.
+            foreach (var (prime, offset) in primesWithOffsets)
+            {
+                var mod = ModFromOffSet(prime, offset);
+                while (result % prime != mod) // the number of times this loop runs is the number ak
+                {
+                    result += product;
+                }
+                product *= prime;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Given an offset, returns the corresponding mod with respect
+        /// to the given prime.
+        /// </summary>
+        /// <example>If <paramref name="prime"/>=13 and <paramref name="offset"/>=1,
+        /// this will return 12 because a positive number that is 1 less than 13
+        /// will have remainder 12 when divided by 13</example>
+        /// <param name="prime">The reference prime</param>
+        /// <param name="offset">The offset</param>
+        /// <returns>The mod</returns>
+        private int ModFromOffSet(int prime, int offset)
+        {
+            if (offset == 0)
+            {
+                return 0;
+            }
+
+            var mod = prime - offset;
+            while (mod < 0) mod += prime; // we'll need positive mods
+
+            return mod;
+        }
+    }
+}
