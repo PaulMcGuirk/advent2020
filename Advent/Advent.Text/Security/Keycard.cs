e68525741183fcdc318d064f1855dd2617f4d8c2
Paul McGuirk <>
2020-12-25 06:00:34 -0600

    Add solutions for day 25

---
+++ Advent/Advent.Text/Security/Keycard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Advent.Text.Security
+{
+    public static class Keycard
+    {
+        public static long GetEncryptionKey(long publicKeyOne, long publicKeyTwo)
+        {
+            var loopSizeOne = ModLog(7, publicKeyOne, 20201227);
+            var result = ModPow(publicKeyTwo, loopSizeOne, 20201227);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Calculate a discrete logarithm. I.e. solves a^x = b mod p
+        /// </summary>
+        /// <param name="base_">The base (a)</param>
+        /// <param name="pow">The result of the exponentiation (b)</param>
+        /// <param name="mod">The divisor (p)</param>
+        /// <returns>The logarithm (x)</returns>
+        private static long ModLog(long base_, long pow, long mod)
+        {
+            var num = 1L;
+            for (var a = 0L; ; a++)
+            {
+                if (num == pow)
+                {
+                    return a; 
+                }
+                num *= base_;
+                num %= mod;
+            }
+        }
+
+        /// <summary>
+        /// Calculate a modular exponentiation, a^b mod p
+        /// </summary>
+        /// <param name="base_">The base (a)</param>
+        /// <param name="exp">The exponentiation (b)</param>
+        /// <param name="mod">The divisor (p)</param>
+        /// <returns>The result</returns>
+        private static long ModPow(long base_, long exp, long mod)
+        {
+            var result = 1L;
+            for (var i = 0; i < exp; i++)
+            {
+                result *= base_;
+                result %= mod;
+            }
+            return result;
+        }
+    }
+
+}
---
+++ Advent/Advent.Text/Solvers/Day25Part01.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Advent.Text.Security;
+
+namespace Advent.Text.Solvers
+{
+    [Solver("25.1")]
+    public class Day25Part01 : Solver
+    {
+        public override object Solve()
+        {
+            var contents = ReadInputFile();
+
+            var publicKeys = contents.Trim().Split('\n').Select(int.Parse).ToArray();
+
+            var result = Keycard.GetEncryptionKey(publicKeys[0], publicKeys[1]);
+            
+            return result;
+        }
+    }
+}
